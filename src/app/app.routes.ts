dbc3ffab8b44022f9bb3c1d1a59c971511ebd58a
hamzakargin <hamzakargin@outlook.com>
2025-04-15 22:58:05 +0300

    tailwindAdded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Route} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Route [] = [
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.registerRoutes)
+  },
+];
