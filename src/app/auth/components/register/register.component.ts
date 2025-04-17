b55d7d83b26464666ffa0f70608bf67537bae6b0
hamzakargin <hamzakargin@outlook.com>
2025-04-17 21:14:23 +0300

    registerPageValidated

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import {Component, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {register} from '../../store/actions';
+import {RegisterRequestInterface} from '../../types/registerRequest.interface';
 
 @Component({
   selector: 'rc-register',
@@ -12,14 +15,19 @@ import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 })
 export class RegisterComponent {
   private fb = inject(FormBuilder);
-  form = this.fb.group({
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-    username: [null, {validators: Validators.required, nonNullable: true}],
-    email: [null, {validators: Validators.required, nonNullable: true}],
-    password: [null, {validators: Validators.required, nonNullable: true}]
-  })
+  private store = inject(Store);
 
   onSubmit() {
     console.log('form', this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue()
+    }
+    this.store.dispatch(register({request}))
   }
 }
